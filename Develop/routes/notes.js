95f9eb4a5bc05b8cba8c8986a5b18bf25aff16a4
Amy Snarr <amy.snarr16@gmail.com>
2023-01-17 14:25:04 -0700

    messages delete when you click the icon

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -12,12 +12,12 @@ notes.get('/', (req, res) =>{
 });
 
 // GET Route for a specific note
-notes.get('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
+notes.get('/:id', (req, res) => {
+    const noteId = req.params.id;
     readFromFile('./db/db.json')
-    .then ((data) => JSON.parse(data))
-    .then ((json) => {
-        const result = json.filter((note) => note.note_id === noteId);
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
         return result.length > 0
         ? res.json (result)
         : res.json('No note found with that ID');
@@ -25,12 +25,12 @@ notes.get('/:note_id', (req, res) => {
 });
 
 // DELETE Route for a specific tip
-notes.delete('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
     readFromFile('./db/db.json')
-    .then ((data) => JSON.parse(data))
-    .then ((json) => {
-        const result = json.filter((note) => note.note_id !== noteId);
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const result = json.filter((note) => note.id !== noteId);
         writeToFile('./db/db.json', result);
         res.json(`Note ${noteId} has been deleted`);
     });
@@ -44,7 +44,7 @@ notes.post('/', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id : uuidv4(),
+            id : uuidv4(),
         };
 
         readAndAppend(newNote, './db/db.json');
